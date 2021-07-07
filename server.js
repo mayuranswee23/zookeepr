1e5fe9c5901fcbbf6849dcece3c65ce4ab1c8f98
Mayuran <mayuranswee23@gmail.com>
2021-07-06 23:53:56 -0400

    using clas lesson

--- server.js
+++ server.js
@@ -104,10 +104,10 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
-// function findById(id, animalsArray) {
-//   const result = animalsArray.filter(animal => animal.id === id)[0];
-//   return result;
-// }
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -117,14 +117,14 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-// app.get('/api/animals/:id', (req, res) => {
-//   const result = findById(req.params.id, animals);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
